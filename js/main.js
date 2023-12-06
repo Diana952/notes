306589be0e12c13542bf8f49e6f86fcce4c62ddf
Diana952 <gadzhievadiana99@gmail.com>
2023-12-07 01:32:03 +0700

    объединить чтоб столбики не улетели

--- js/main.js
+++ js/main.js
@@ -6,17 +6,163 @@ Vue.component('desk', {
 <div>
      <createTask></createTask>
      <div>
-         <col1></col1>
-         <col2></col2>
-         <col3></col3>
+     <div class="col">
+     <h2>0% выполнено</h2>
+     <div>
+         <div v-for="task in firstColTasks" class="col-item">
+             <p class="task-list-name">{{ task.list_name }}</p>
+             <div v-for="t in task.tasks">
+                     <p @click="doneTask(task, t)" v-bind:class="{ done: t.status }"> {{ t.task }}</p>
+             </div>
+         </div>
+     
+     </div>
+ </div>
+     <div class="col">
+
+<h2>50% выполнено</h2>
+ <div v-bind:class="{ disables: errors}">
+         <div v-for="task in secondDoneTasks" class="col-item">
+             <p class="task-list-name">{{ task.list_name }}</p>
+             <div v-for="t in task.tasks">
+                     <p @click="allDoneTask(task, t)" v-bind:class="{ done: t.status }"> {{ t.task }}</p>                       
+             </div>
+         </div>
+ </div>
+</div>
+     <div class="col">
+ <h2>100% выполнено</h2>
+    <div>
+         <div v-if='allDoneTasks' v-for="task in allDoneTasks" class="col-item">
+             <p class="task-list-name">{{ task.list_name }}</p>
+             <div v-for="t in task.tasks">
+                     <p v-bind:class="{ done: t.status }"> {{ t.task }}</p>
+             </div>
+             <p>{{ task.date }}</p>
+         </div>
+     </div>
+     </div>
      </div>
 </div>
-    </div>
+</div>
     `,
 
-})
+    data() {
+        return {
+            firstColTasks:[],
+            secondDoneTasks: [],
+            allDoneTasks: [],
+            errors: []
+        }
+    },
+    methods: {
+        doneTask(list, task){
+                if (task.status === false) {
+                    task.status = true;
+                    list.done++;
+                }
 
+                let count = 0;
 
+                for (let i = 0; i < 5; ++i) {
+                    if (list.tasks[i].task !== null) {
+                        count++;
+                    }
+                }
+
+                if (list.done / count * 100 >= 50 && this.secondDoneTasks.length < 5) {
+                    this.secondDoneTasks.push(list);
+                    this.firstColTasks.splice(this.firstColTasks.indexOf(list), 1);
+                }
+                else if (this.errors.length < 1) this.errors.push('Вы еще не предыдущие задачи, сначала выполните задачи из второго столбика');
+        },
+
+        allDoneTask(list, task){
+            if (task.status === false) {
+                task.status += 1;
+                list.done++;
+            }
+
+            let count = 0;
+            for (let i = 0; i < 5; ++i) {
+                if (list.tasks[i].task !== null) {
+                    count++;
+                }
+            }
+
+            if ((list.done / count) * 100 === 100) {
+                this.allDoneTasks.push(list);
+                this.secondDoneTasks.splice(this.secondDoneTasks.indexOf(list), 1);
+            }
+        }
+    },
+    mounted() {
+        this.firstColTasks = JSON.parse(localStorage.getItem("firstColTasks")) || [];
+        {
+            eventBus.$on('task_list', data => {
+                    if (this.firstColTasks.length < 3) {
+                        this.firstColTasks.push(data);
+                    } else if (this.errors.length < 1){
+                        this.errors.push('Вы еще не выполнили предыдущие задачи, сначала выполните задачи из 1 столбика');
+                    }
+                }
+            )
+        }
+
+        this.secondDoneTasks = JSON.parse(localStorage.getItem('secondDoneTasks')) || [];
+        {
+            eventBus.$on('semiDone', list => {
+                    if (this.secondDoneTasks.length < 5) {
+                        this.secondDoneTasks.push(list);
+
+                    } else if (this.errors.length < 1){
+                        this.errors.push('Вы еще не выполнили предыдущие задачи');
+                    }
+                }
+            )
+        }
+
+        this.allDoneTasks = JSON.parse(localStorage.getItem("allDoneTasks")) || [];
+        {
+            eventBus.$on('allDone', data => {
+                    data.date = new Date();
+                    this.allDoneTasks.push(data);
+                    localStorage.setItem('allDoneTasks', JSON.stringify(this.allDoneTasks));
+                }
+            )
+        }
+    },
+    watch: {
+        firstColTasks: {
+            handler(newValue, oldValue) {
+                if (this.firstColTasks.length < 3){
+                    this.errors.splice(0, this.errors.length);
+                }
+                localStorage.setItem('firstColTasks', JSON.stringify(newValue));
+            },
+            deep: true
+        },
+
+        secondDoneTasks: {
+            handler(newValue, oldValue) {
+                if (this.secondDoneTasks.length < 5){
+                    this.errors.splice(0, this.errors.length);
+                }
+                localStorage.setItem('secondDoneTasks', JSON.stringify(newValue));
+            },
+            deep: true
+        },
+        allDoneTask: {
+            handler(newValue, oldValue) {
+                localStorage.setItem('allDoneTask', JSON.stringify(newValue));
+            },
+            deep: true
+        },
+    }
+
+}) 
+
+/*
 Vue.component('col3', {
     template: `
             <div class="col">
@@ -62,7 +208,9 @@ Vue.component('col3', {
         }
     }
 })
+*/
 
+/*
 Vue.component('col2', {
     template: `
         
@@ -136,8 +284,9 @@ Vue.component('col2', {
     }
 
 })
+*/
 
-
+/*
 Vue.component('col1', {
     template: `
         <div class="col">
@@ -145,7 +294,7 @@ Vue.component('col1', {
         <p>{{ error }}</p>
         </div>
         <h2>0% выполнено</h2>
-        <div v-bind:class="{ disabled: block }">
+        <div>
                 
                     <div v-for="task in firstColTasks" class="col-item">
                         <p class="task-list-name">{{ task.list_name }}</p>
@@ -210,6 +359,7 @@ Vue.component('col1', {
         }
     }
 })
+*/
 
 
 Vue.component('createTask', {
